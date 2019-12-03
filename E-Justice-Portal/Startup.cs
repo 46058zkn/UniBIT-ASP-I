e9cefd2f3ef170bd0f73eced945dd07403e2acc6
Красимир Банчев <46058z@unibit.bg>
2019-12-03 08:41:22 +0200

    Версия 1.11

--- E-Justice-Portal/Startup.cs
+++ E-Justice-Portal/Startup.cs
@@ -33,6 +33,12 @@ namespace E_Justice_Portal
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
