652d9925b819ab9bcf0cf08df020b0b2fd5b585b
Красимир Банчев <46058z@unibit.bg>
2019-12-03 11:30:32 +0200

    Версия 1.0

--- E-Justice-Portal/Pages/ImageUpload.cshtml.cs
+++ E-Justice-Portal/Pages/ImageUpload.cshtml.cs
@@ -13,22 +13,16 @@ namespace E_Justice_Portal.Pages
 {
     public class ImageUploadFormModel : PageModel
     {
-        //COMMENT:static declaration of the CloudBlobClient so we can //interact with our storage service.
-        private static CloudBlobClient blobClient;
+        private static CloudBlobClient _blobClient;
 
-        //COMMENT:constant to hold the blob container name
-        private const string BLOB_CONTAINER_NAME = "photogallery-images";
+        private const string BlobContainerName = "photogallery-images";
 
-        //COMMENT:static declaration of CloudBlobContainer which will store //a reference to the blobcontainer that we created earlier
-        private static CloudBlobContainer blobContainer;
+        private static CloudBlobContainer _blobContainer;
 
-        //COMMENT:declaration of ApplicationDbContext for an instance of our //database context.
         private ApplicationDbContext _context;
 
-        //COMMENT:setup our configuration so that we can have access to the //Azure storage connection string later.
         public IConfiguration _configuration;
 
-        //COMMENT:This is a property for GalleryImage with a //BindPropertyAttribute. This will automatically bind the data from //the form to the properties of GalleryImage when the form is //submitted.
         [BindProperty]
         public GalleryImage GalleryImage { get; set; }
 
@@ -41,51 +35,40 @@ namespace E_Justice_Portal.Pages
 
         public async Task OnGetAsync()
         {
-            // COMMENT:Retrieve storage account information from connection string
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_configuration.GetConnectionString("AzureStorageConnectionString"));
 
-            // COMMENT:Create a blob client for interacting with the storage //blob service.
-            blobClient = storageAccount.CreateCloudBlobClient();
+            _blobClient = storageAccount.CreateCloudBlobClient();
 
-            //COMMENT:this gets a reference to the container that we created earlier
-            blobContainer = blobClient.GetContainerReference(BLOB_CONTAINER_NAME);
+            _blobContainer = _blobClient.GetContainerReference(BlobContainerName);
 
-            //COMMENT:this will create a container with the name that we passed //above in the event that the container doesn't exist.
-            await blobContainer.CreateIfNotExistsAsync();
+            await _blobContainer.CreateIfNotExistsAsync();
 
-            //COMMENT:set permissions to public access
-            await blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            await _blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
         }
 
         public async Task<IActionResult> OnPost(IFormCollection form)
         {
             try
             {
-                //COMMENT:we are only allowing one upload, so just get the first one //in the file collection.
                 var file = form.Files.FirstOrDefault();
 
-                //COMMENT:this block will store the image into the blobContainer //container
-                CloudBlockBlob blob = blobContainer.GetBlockBlobReference(file?.FileName);
+                CloudBlockBlob blob = _blobContainer.GetBlockBlobReference(file?.FileName);
 
                 blob.Properties.ContentType = file?.ContentType;
 
                 await blob.UploadFromStreamAsync(file?.OpenReadStream());
 
-                //COMMENT:set the url of the image that we just uploaded
-                GalleryImage.URL = $"{blobContainer.StorageUri.PrimaryUri}/{file?.FileName}";
+                GalleryImage.URL = $"{_blobContainer.StorageUri.PrimaryUri}/{file?.FileName}";
 
-                //COMMENT:add a GalleryImage to the database and save it.
                 _context.GalleryImages.Add(GalleryImage);
 
                 await _context.SaveChangesAsync();
 
-                //COMMENT:if we got this far, it was successful so let's tell the user.
-                //set a tempData variable to a success string. we will use this //variable after the redirect to the gallery.
                 TempData["SuccessMessage"] = "Успешно беше качен файл!!";
 
                 return RedirectToPage("ImageGallery");
             }
-            catch (Exception ex)
+            catch
             {
                 return RedirectToPage("Error");
             }
